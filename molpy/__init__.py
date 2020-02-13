69856197fcb293fd0fdfcc81de7d47c8d0c38058
DrKakularam <kkumarreddy@uohyd.ac.in>
2020-02-13 07:19:08 +0100

    done

--- molpy/__init__.py
+++ molpy/__init__.py
@@ -5,9 +5,14 @@ Workshop package
 
 # Add imports here
 from .molpy import *
+from . import util 
+from . import data
 
 # Handle versioneer
 from ._version import get_versions
+diff --git a/molpy/data/__init__.py b/molpy/data/__init__.py
+newfile mode 100644
+index 00000
 versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
